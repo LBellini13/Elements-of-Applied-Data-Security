1f9911a7fa7d76510a6b41c04590595ab9ad2148
Leonardo Bellini <lebe00@gmail.com>
2024-04-30 16:32:51 +0200

    bonus task first

--- Block Ciphers/blockciphers.py
+++ Block Ciphers/blockciphers.py
@@ -37,3 +37,64 @@ def aes_mcs_confusion(ref_key, key_length, ref_plaintext, ref_ciphertext, it):
         ciphertext = aes.encrypt(ref_plaintext)
         dist.append(hamming(ref_ciphertext, ciphertext)/8/AES.block_size*100)
     return dist
+
+def key_scheduling_algorithm(key):
+    p = list(range(256))
+    j = 0
+    for i in range(256):
+        j = (j + p[i] + key[i % len(key)]) % 256
+        p[i], p[j] = p[j], p[i]
+    return p
+
+
+class RC4():
+    def __init__(self, key, drop = None):
+        self.key = key
+        self.drop = drop
+        self.dropped_bytes = 0
+        self.i, self.j = 0, 0
+        self.p = key_scheduling_algorithm(self.key)
+        print(self.p)
+        self.out = None
+        self.out_int = None
+
+    def prga(self):
+        self.i = (self.i + 1) % 256
+        self.j = (self.j + self.p[self.i]) % 256
+        self.p[self.i], self.p[self.j] = self.p[self.j], self.p[self.i]
+        self.out_int = self.p[(self.p[self.i] + self.p[self.j]) % 256]
+
+    def drop_bytes(self):
+        if self.drop is not None:
+            while self.dropped_bytes < self.drop:
+                self.prga()
+                self.dropped_bytes += 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.drop_bytes()
+        self.prga()
+        self.out = self.out_int.to_bytes((self.out_int // 256 ) + 1, byteorder='big')
+        return self.out
+    
+    def run_steps(self, n):
+        keystream = []
+        self.drop_bytes()
+        for _ in range(n):
+            self.prga()
+            self.out = self.out_int.to_bytes((self.out_int // 256 ) + 1, byteorder='big')
+            keystream.append(self.out_int)
+        return bytes(keystream)
+    
+    def encrypt(self, plaintext):
+        keystream = self.run_steps(len(plaintext))
+        ciphertext = bytes([pb ^ kb for pb, kb in zip(plaintext, keystream)])
+        return ciphertext
+    
+    def decrypt(self, ciphertext):
+        keystream = self.run_steps(len(ciphertext))
+        plaintext = bytes([cb ^ kb for cb, kb in zip(ciphertext, keystream)])
+        return plaintext
+    
